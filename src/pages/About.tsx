3a6607609a10f4019993d13c10db3bd5a4b72ac6
doandrade <doandrade@tce.pb.gov.br>
2025-02-06 14:53:22 +0000

    Cria o projeto e instala as dependências do mesmo.

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Sobre Mim</h1>
+        <p className="text-lg text-gray-600">Sou um desenvolvedor apaixonado por tecnologia.</p>
+      </div>
+    );
+  }
+  
+  export default About;
+  
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Página Inicial</h1>
+        <p className="text-lg text-gray-600">Bem-vindo ao meu portfólio!</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
